d41536deb35116143693bf8f024416f0e7ca900d
Stevenkwizera06 <kwisteo6@gmail.com>
2025-11-01 07:30:43 +0200

    Initial commit: W01 Home Page assignment - Complete homepage with About Me and Web Dev Resources sections

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Get the current year for the copyright
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Get the last modified date
+document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
+
